504588f01c3e8c4c27e85c0f0f604a9f07825e89
sagar1712 <sagars536335@gmail.com>
2024-06-18 00:32:48 +0530

    fix: fixed minor build issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { socialMedia } from '@/data';
+import Image from 'next/image';
 import { FaLocationArrow } from 'react-icons/fa6';
 import MagicButton from './ui/MagicButton';
-
 const Footer = () => {
 	return (
 		<footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
@@ -11,7 +11,7 @@ const Footer = () => {
 					presence to the next level?
 				</h1>
 				<p className="text-white-200 md:mt-10 my-5 text-center">
-					Reach out to me today and let's discuss how I can help you achieve
+					Reach out to me today and let&apos;s discuss how I can help you achieve
 					your goals.
 				</p>
 				<a href="sagars536335@gmail.com">
@@ -34,7 +34,7 @@ const Footer = () => {
 							key={profile.id}
 							className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
 						>
-							<img src={profile.img} alt={profile.id} width={20} height={20} />
+							<Image src={profile.img} alt={profile.id.toString()} width={20} height={20} />
 						</div>
 					))}
 				</div>
