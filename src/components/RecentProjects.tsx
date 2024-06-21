50d3039fbd7f1146bfa0462d800e087f98110316
sagar1712 <sagars536335@gmail.com>
2024-06-22 00:44:28 +0530

    fix: navigation to github and live site fix

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -13,12 +13,12 @@ const RecentProjects = () => {
 				<span className="text-purple">recent projects</span>
 			</h1>
 			<div className="flex flex-wrap items-center justify-center p-4 gap-y-8 gap-x-24 mt-10">
-				{projects.map(({ id, title, des, img, iconLists, link }) => (
+				{projects.map(({ id, title, des, img, iconLists, githubLink, link }) => (
 					<div
 						className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
 						key={id}
 					>
-						<PinContainer title={link} href={link}>
+						<PinContainer title={githubLink} href={githubLink}>
 							<div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
 								<div
 									className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]"
@@ -26,11 +26,7 @@ const RecentProjects = () => {
 								>
 									<img src="/bg.png" alt="bg-img" />
 								</div>
-								<img
-									src={img}
-									alt="cover"
-									className="z-10 absolute bottom-0"
-								/>
+								<img src={img} alt="cover" className="z-10 absolute bottom-0" />
 							</div>
 
 							<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
@@ -57,17 +53,22 @@ const RecentProjects = () => {
 												transform: `translateX(-${5 * index + 2}px)`,
 											}}
 										>
-											<img src={icon} alt="icon5" className="p-2" />
+											<img src={icon} alt="icon" className="p-2" />
 										</div>
 									))}
 								</div>
 
-								<div className="flex justify-center items-center">
+								<a
+									href={link}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="flex justify-center items-center"
+								>
 									<p className="flex lg:text-xl md:text-xs text-sm text-purple">
 										Check Live Site
 									</p>
 									<FaLocationArrow className="ms-3" color="#CBACF9" />
-								</div>
+								</a>
 							</div>
 						</PinContainer>
 					</div>
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
 export const navItems = [
     { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
+    { name: "Projects", link: "#projec3ts" },
     { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
@@ -72,11 +72,12 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      title: "Discord Clone",
+      des: "A complete communication webapp with the ease of creating your own custom servers and channels and also a platform to jump on video meets with friends and professionals",
+      img: "/p1.png",
+      iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg"],
+      githubLink: "https://github.com/sagar1712/discord-clone",
+      link: "https://sagar-discord-clone.vercel.app/"
     },
     {
       id: 2,
@@ -84,23 +85,8 @@ export const navItems = [
       des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
       img: "/p2.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      githubLink: "https://github.com/adrianhajdin/zoom-clone",
       link: "https://github.com/adrianhajdin/zoom-clone",
-    },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
     },
   ];
   
